6dc10c5ef31550deac60700137e2dce80e28f8eb
irfanalarib <irfanalarib7@gmail.com>
2025-04-14 15:49:26 +0700

    menampilkan dan menambahkan array

--- Array.cpp
+++ Array.cpp
@@ -1,2 +1,40 @@
 #include <iostream>
 using namespace std;
+
+string matkul[] = {"pemograman dasar" , "Algoritma"};
+
+float nilaiakhir[2] = {1 , 2.5};
+
+char grade [5];
+
+int main()
+{
+    cout << "Nama matakuliah : " << matkul[1] << endl;
+    matkul[0] = "Logika Teknik Pemograman";
+    cout << "Nama matakuliah : " << matkul[0] << endl;
+
+    for( int i = 0; i < 2; i++) 
+    {
+        cout << "Nilai akhir ke-" << i+i << "=" << nilaiakhir[i] << endl;
+    }
+
+
+    cout << "Input Grade" << endl;
+    for(int i = 0; i < 5; i++)
+    {
+        cout << "Masukkan Grade ";
+        cin >> grade [i];
+    }
+    cout << endl;
+    cout << "Tampilkan Grade" << endl;
+    cout << endl;
+
+    for  (int i= 0; i < 5; i++)
+    {
+        cout << "Grade = " << grade [i] <<endl;
+
+    }
+
+
+
+}
